8717b7c3a8ceeca9c09cdf098d46283db2805dba
swapna2021 <swapna.motupally@gmail.com>
2025-09-16 16:08:05 +0530

    springboot app commiting

---
+++ GaminApp_team1/src/main/java/com/team1/GaminAppTeam1Application.java
@@ -0,0 +1,13 @@
+package com.team1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaminAppTeam1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaminAppTeam1Application.class, args);
+	}
+
+}
---
+++ GaminApp_team1/src/test/java/com/team1/GaminAppTeam1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaminAppTeam1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
